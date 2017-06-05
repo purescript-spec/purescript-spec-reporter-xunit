21c65825b789e5b00c45f6b8d381cb5d39f840e7
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-06-05 19:07:48 +0200

    Fix warnings

--- test/Test/Spec/Fixtures.purs
+++ test/Test/Spec/Fixtures.purs
@@ -1,16 +1,17 @@
 module Test.Spec.Fixtures where
 
 import Prelude
-
-import Test.Spec (describe, it, pending)
+import Test.Spec (Spec, describe, it, pending)
 import Test.Spec.Assertions (shouldEqual)
 
+successTest :: forall e. Spec e Unit
 successTest =
   describe "a" do
     describe "b" do
       it "works" do
         1 `shouldEqual` 1
 
+sharedDescribeTest :: forall e. Spec e Unit
 sharedDescribeTest =
   describe "a" do
     describe "b" do
@@ -20,6 +21,8 @@ sharedDescribeTest =
       it "also works" do
         1 `shouldEqual` 1
 
+failureTest :: forall e. Spec e Unit
 failureTest = it "fails" $ 1 `shouldEqual` 2
 
+pendingTest :: forall e. Spec e Unit
 pendingTest = pending "is not written yet"
