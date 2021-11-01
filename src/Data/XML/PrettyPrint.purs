a5b9f12d768e64426c3dcb892b9486991709689e
Ben Ford <ben@commando.dev>
2021-11-01 16:33:16 +0000

    Bump versions and change error to failure

--- src/Data/XML/PrettyPrint.purs
+++ src/Data/XML/PrettyPrint.purs
@@ -1,12 +1,12 @@
-module Data.XML.PrettyPrint (
-  Indent(),
-  print
+module Data.XML.PrettyPrint
+  ( Indent()
+  , print
   ) where
 
 import Prelude
-import Data.XML as XML
-import Control.Monad.State (runStateT, get, lift, modify, StateT)
-import Control.Monad.Writer (execWriter, tell, Writer)
+
+import Control.Monad.State (StateT, get, lift, modify, runStateT)
+import Control.Monad.Writer (Writer, execWriter, tell)
 import Data.Array (replicate)
 import Data.Either (Either(Right, Left))
 import Data.Foldable (foldl, sequence_)
@@ -14,6 +14,7 @@ import Data.String (Pattern(Pattern), split)
 import Data.String.CodeUnits (fromCharArray)
 import Data.String.Regex (regex, replace')
 import Data.String.Regex.Flags (RegexFlags(RegexFlags))
+import Data.XML as XML
 
 type Indent = Int
 type CurrentIndent = Int
@@ -24,52 +25,54 @@ data PrinterState = PrinterState Indent CurrentIndent
 
 type Printer v = StateT PrinterState (Writer String) v
 
-indent :: Printer Unit
-indent = void $ modify $ \(PrinterState i ci) -> PrinterState i (ci + 1)
+indent ∷ Printer Unit
+indent = void $ modify $ \(PrinterState i ci) → PrinterState i (ci + 1)
 
-dedent :: Printer Unit
-dedent = void $ modify $ \(PrinterState i ci) -> PrinterState i (ci - 1)
+dedent ∷ Printer Unit
+dedent = void $ modify $ \(PrinterState i ci) → PrinterState i (ci - 1)
 
-indentSpaces :: Printer String
+indentSpaces ∷ Printer String
 indentSpaces = do
-  (PrinterState indentWidth currentIndent) <- get
+  (PrinterState indentWidth currentIndent) ← get
   pure $ fromCharArray $ replicate (indentWidth * currentIndent) ' '
 
-appendLine :: String -> Printer Unit
+appendLine ∷ String → Printer Unit
 appendLine "" = lift $ tell "\n"
 appendLine s = do
-  spaces <- indentSpaces
+  spaces ← indentSpaces
   lift $ tell $ spaces <> s <> "\n"
 
-enclosed :: String -> String -> String -> String
+enclosed ∷ String → String → String → String
 enclosed before after contents = before <> contents <> after
 
-openTag :: String -> Array XML.Attr -> String
+openTag ∷ String → Array XML.Attr → String
 openTag contents attrs = enclosed "<" ">" (contents <> showAttrs attrs)
 
-closeTag :: String -> String
+closeTag ∷ String → String
 closeTag contents = enclosed "</" ">" contents
 
-escape :: String -> String
+escape ∷ String → String
 escape s =
   case regex "[<>\t\n\r\"]" flags of
-    Left _ -> s
-    Right exp -> replace' exp replacer s
-  where replacer "<" _ = "&lt;"
-        replacer ">" _ = "&gt;"
-        replacer "\"" _ = "&quot;"
-        replacer "\t" _ = ""
-        replacer "\r" _ = ""
-        replacer s' _ = s'
-        flags = RegexFlags {
-          unicode: false,
-          sticky: false,
-          multiline: false,
-          ignoreCase: false,
-          global: true
-        }
+    Left _ → s
+    Right exp → replace' exp replacer s
+  where
+  replacer "<" _ = "&lt;"
+  replacer ">" _ = "&gt;"
+  replacer "\"" _ = "&quot;"
+  replacer "\t" _ = ""
+  replacer "\r" _ = ""
+  replacer s' _ = s'
+  flags = RegexFlags
+    { unicode: false
+    , sticky: false
+    , multiline: false
+    , ignoreCase: false
+    , global: true
+    , dotAll: false
+    }
 
-printNode :: XML.Node -> Printer Unit
+printNode ∷ XML.Node → Printer Unit
 printNode (XML.Comment s) = appendLine $ enclosed "<!-- " " -->" s
 printNode (XML.Text s) = do
   sequence_ $ map appendLine $ split (Pattern "\n") $ escape s
@@ -82,15 +85,16 @@ printNode (XML.Element tagName attrs nodes) = do
   dedent
   appendLine $ closeTag tagName
 
-showAttrs :: Array XML.Attr -> String
+showAttrs ∷ Array XML.Attr → String
 showAttrs as = foldl iter "" as
-  where iter acc (XML.Attr key value) = acc <> " " <> key <> "=\"" <> (escape value) <> "\""
+  where
+  iter acc (XML.Attr key value) = acc <> " " <> key <> "=\"" <> (escape value) <> "\""
 
-printDocument :: XML.Document -> Printer Unit
+printDocument ∷ XML.Document → Printer Unit
 printDocument (XML.Document version encoding node) = do
-  appendLine $ "<?xml" <> (showAttrs [XML.Attr "version" version, XML.Attr "encoding" encoding]) <> "?>"
+  appendLine $ "<?xml" <> (showAttrs [ XML.Attr "version" version, XML.Attr "encoding" encoding ]) <> "?>"
   printNode node
 
-print :: Indent -> XML.Document -> String
+print ∷ Indent → XML.Document → String
 print indentWidth doc =
   execWriter (runStateT (printDocument doc) (PrinterState indentWidth 0))
--- src/Test/Spec/Reporter/Xunit.purs
+++ src/Test/Spec/Reporter/Xunit.purs
@@ -1,7 +1,7 @@
 module Test.Spec.Reporter.Xunit
-       ( xunitReporter
-       , defaultOptions
-       ) where
+  ( xunitReporter
+  , defaultOptions
+  ) where
 
 import Prelude
 
@@ -13,7 +13,7 @@ import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Exception (message)
 import Node.Encoding (Encoding(UTF8))
-import Node.FS.Sync (writeTextFile, exists, unlink)
+import Node.FS.Sync (exists, unlink, writeTextFile)
 import Node.Path (FilePath)
 import Pipes.Prelude (chain)
 import Test.Spec as S
@@ -21,51 +21,52 @@ import Test.Spec.Result as R
 import Test.Spec.Runner (Reporter)
 import Test.Spec.Runner.Event as Event
 
-encodeResult :: R.Result -> Array XML.Node
+encodeResult ∷ R.Result → Array XML.Node
 encodeResult (R.Success _speed _ms) = []
 encodeResult (R.Failure err) =
-  [XML.Element
-    "error"
-    [XML.Attr "message" (message err)]
-    [XML.Text (show err)]]
+  [ XML.Element
+      "failure"
+      [ XML.Attr "message" (message err) ]
+      [ XML.Text (show err) ]
+  ]
 
-encodeGroup :: S.Tree Void R.Result -> XML.Node
+encodeGroup ∷ S.Tree Void R.Result → XML.Node
 encodeGroup (S.Node (Left name) groups) =
-  XML.Element "testsuite" [XML.Attr "name" name] $ map encodeGroup groups
-encodeGroup (S.Node (Right action) groups) =
+  XML.Element "testsuite" [ XML.Attr "name" name ] $ map encodeGroup groups
+encodeGroup (S.Node (Right _action) groups) =
   XML.Element "testsuite" [] $ map encodeGroup groups
 encodeGroup (S.Leaf name (Just result)) =
-  XML.Element "testcase" [XML.Attr "name" name] (encodeResult result)
+  XML.Element "testcase" [ XML.Attr "name" name ] (encodeResult result)
 encodeGroup (S.Leaf name Nothing) =
-  XML.Element "testcase" [XML.Attr "name" name] [XML.Element "skipped" [] []]
+  XML.Element "testcase" [ XML.Attr "name" name ] [ XML.Element "skipped" [] [] ]
 
-encodeSuite :: Array (S.Tree Void R.Result) -> XML.Document
+encodeSuite ∷ Array (S.Tree Void R.Result) → XML.Document
 encodeSuite groups = XML.Document "1.0" "UTF-8" $ XML.Element "testsuite" [] $ map encodeGroup groups
 
-removeIfExists :: FilePath -> Effect Unit
+removeIfExists ∷ FilePath → Effect Unit
 removeIfExists path = do
-  e <- exists path
+  e ← exists path
   when e $ unlink path
 
-type XunitReporterOptions = { outputPath :: FilePath
-                            , indentation :: Int
-                            }
+type XunitReporterOptions =
+  { outputPath ∷ FilePath
+  , indentation ∷ Int
+  }
 
-defaultOptions :: XunitReporterOptions
+defaultOptions ∷ XunitReporterOptions
 defaultOptions = { indentation: 2, outputPath: "output/test.xml" }
-
 
 -- | Outputs an XML file at a specified path that can be consumed by Xunit
 -- | readers, e.g. the Jenkins plugin.
-xunitReporter :: XunitReporterOptions -> Reporter
+xunitReporter ∷ XunitReporterOptions → Reporter
 xunitReporter options = chain handleEvent
   where
 
-    handleEvent = case _ of
-      Event.End results -> void $ liftEffect $ summarize results
-      _                 -> pure unit
+  handleEvent = case _ of
+    Event.End results → void $ liftEffect $ summarize results
+    _ → pure unit
 
-    summarize groups = do
-      let xml = encodeSuite groups
-      removeIfExists options.outputPath
-      writeTextFile UTF8 options.outputPath (print options.indentation xml)
+  summarize groups = do
+    let xml = encodeSuite groups
+    removeIfExists options.outputPath
+    writeTextFile UTF8 options.outputPath (print options.indentation xml)
